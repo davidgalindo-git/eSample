4ccd95d6f84cd49b3840af91b598608647edf8f4
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-04 16:00:25 +0100

    refactor:rename vars

--- e-sample-app/src/components/SearchBar.vue
+++ e-sample-app/src/components/SearchBar.vue
@@ -1,18 +1,18 @@
 <script setup>
 import {ref} from 'vue'
 
+const input = ref(null)
 const keyword = ref(null)
-const filters = ref(null)
 
-function submitKeyword(keyword) {
-  filters.value = keyword
+function submitKeyword(input) {
+  keyword.value = input
 }
 </script>
 
 <template>
   <p>Research samples</p>
-  <input v-model="keyword" placeholder="Insert keyword" @keyup.enter="submitKeyword(keyword)"/>
-  <p>{{ filters }}</p>
+  <input v-model="input" placeholder="Insert keyword" @keyup.enter="submitKeyword(input)"/>
+  <p>{{ keyword }}</p>
 </template>
 
 <style scoped>
