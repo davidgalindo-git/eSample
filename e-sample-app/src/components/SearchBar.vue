9d5bdbc1e93cf91c7fbc71f8c64836ea7459998d
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-04 15:56:21 +0100

    feat:keyword search bar backend store logic

--- e-sample-app/src/components/SearchBar.vue
+++ e-sample-app/src/components/SearchBar.vue
@@ -2,11 +2,17 @@
 import {ref} from 'vue'
 
 const keyword = ref(null)
+const filters = ref(null)
+
+function submitKeyword(keyword) {
+  filters.value = keyword
+}
 </script>
 
 <template>
   <p>Research samples</p>
-  <input v-model="keyword" placeholder="Insert keyword" @keyup.enter="submitKeyword"/>
+  <input v-model="keyword" placeholder="Insert keyword" @keyup.enter="submitKeyword(keyword)"/>
+  <p>{{ filters }}</p>
 </template>
 
 <style scoped>
