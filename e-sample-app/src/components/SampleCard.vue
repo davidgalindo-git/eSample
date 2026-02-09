1de8c92130318b0fcc394464adec222ac9c3369c
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-09 15:11:34 +0100

    feat:sample display container

---
+++ e-sample-app/src/components/SampleCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  sample:{
+    type: Object,
+    required: true
+  }
+})
+
+console.log("Sample reçu:", props.sample)
+</script>
+
+<template>
+  <div class="sample-card">
+    <p class="sample-name">{{ sample.name }}</p>
+    <small>ID: {{ sample.id }}</small>
+    <span>tags: {{ sample.tags }}</span>
+  </div>
+</template>
+
+<style scoped>
+.sample-card{
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #eee;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.sample-name{
+  font-weight: bold;
+  font-size: 1rem;
+  margin-bottom: 10px;
+}
+
+</style>
--- e-sample-app/src/components/SearchBar.vue
+++ e-sample-app/src/components/SearchBar.vue
@@ -1,13 +1,13 @@
 <script setup>
 import {ref} from 'vue'
 import { useSampleAPI } from '../stores/sampleStore.js'
+import SampleCard from "./SampleCard.vue";
 
 const sampleStore = useSampleAPI()
-
 const keyword = ref(null)
 
-function submitKeyword(keyword) {
-  sampleStore.searchSamples(keyword)
+const submitKeyword = async (keyword) => {
+  await sampleStore.searchSamples(keyword)
 }
 </script>
 
@@ -15,7 +15,14 @@ function submitKeyword(keyword) {
   <p>Research samples</p>
   <input v-model="keyword" placeholder="Insert keyword" @keyup.enter="submitKeyword(keyword)"/>
   <p>{{ keyword }}</p>
-  <p>{{sampleStore.samples}}</p>
+  <div class="sample-container">
+    <p>nb of samples: {{ sampleStore.samples.value.length }}</p>
+    <SampleCard
+      v-for="sample in sampleStore.samples.value"
+      :key="sample.id"
+      :sample="sample"
+      />
+  </div>
 </template>
 
 <style scoped>
