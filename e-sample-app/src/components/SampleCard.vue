b0b3622631a7cb0f173a7be6fd488a7988a7e525
David Galindo <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-10 12:10:59 +0100

    Feature/sample display container (#24)
    * feat:sample display container
    
    * chore:update readme for tests
    
    * refactor:del log line
    
    * refactor:display log update
    
    * feat:pinia config
    
    * feat:sample safety check

---
+++ e-sample-app/src/components/SampleCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  sample:{
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="sample-card">
+    <p class="sample-name">{{ sample.name }}</p>
+    <small>ID: {{ sample.id }}</small>
+    <span>tags: {{ sample.tags }}</span>
+  </div>
+</template>
+
+<style scoped>
+.sample-card{
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #eee;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.sample-name{
+  font-weight: bold;
+  font-size: 1rem;
+  margin-bottom: 10px;
+}
+
+</style>
--- e-sample-app/src/components/SearchBar.vue
+++ e-sample-app/src/components/SearchBar.vue
@@ -1,21 +1,28 @@
 <script setup>
 import {ref} from 'vue'
 import { useSampleAPI } from '../stores/sampleStore.js'
+import SampleCard from "./SampleCard.vue";
 
 const sampleStore = useSampleAPI()
-
 const keyword = ref(null)
 
-function submitKeyword(keyword) {
-  sampleStore.searchSamples(keyword)
+const submitKeyword = async (keyword) => {
+  await sampleStore.searchSamples(keyword)
 }
 </script>
 
 <template>
   <p>Research samples</p>
   <input v-model="keyword" placeholder="Insert keyword" @keyup.enter="submitKeyword(keyword)"/>
-  <p>{{ keyword }}</p>
-  <p>{{sampleStore.samples}}</p>
+  <p>keyword: {{ keyword }}</p>
+  <div class="sample-container" v-if="sampleStore.samples.value.length > 0">
+    <SampleCard
+      v-for="sample in sampleStore.samples.value"
+      :key="sample.id"
+      :sample="sample"
+      />
+  </div>
+  <p>nb of samples: {{ sampleStore.samples.value.length }}</p>
 </template>
 
 <style scoped>
