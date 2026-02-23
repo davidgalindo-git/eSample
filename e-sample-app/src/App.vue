6072f2aa875bc694adbbccc06ae960df1a78e7ed
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-23 15:53:21 +0100

    feat:"toggle likes display logic"

--- e-sample-app/src/App.vue
+++ e-sample-app/src/App.vue
@@ -1,18 +1,28 @@
 <script setup>
+import {ref} from "vue";
+
 import {useSampleAPI} from "./stores/sampleStore.js";
 
 import SearchBar from "./components/SearchBar.vue";
 import SampleContainer from "./components/SampleContainer.vue";
 import LikedSamplesList from "./components/LikedSamplesList.vue";
+import ShowLikesButton from "./components/ShowLikesButton.vue";
 
 const sampleStore =useSampleAPI()
+
+const isVisible = ref(false)
+
+const toggleLikes = (value) => {
+  isVisible.value = value
+}
 </script>
 
 <template>
   <div id="app">
     <h1>eSample</h1>
     <SearchBar />
-    <LikedSamplesList />
+    <ShowLikesButton :is-visible="isVisible" @toggle-display="toggleLikes"/>
+    <LikedSamplesList v-show="isVisible"/>
     <p>Nb of samples: {{ sampleStore.samples.value.length }}</p>
     <SampleContainer :samples="sampleStore.samples.value"/>
   </div>
