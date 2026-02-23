8bdb0817d3ed2d5d2d81a501ae4ebd846ccd7bcb
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-23 14:03:20 +0100

    refactor:"fix var refs and added likedList"

--- e-sample-app/src/stores/likedStore.js
+++ e-sample-app/src/stores/likedStore.js
@@ -1,15 +1,16 @@
 import { defineStore } from "pinia";
-import { ref, watch } from "vue";
+import {computed, ref, watch} from "vue";
 
 export const useLiked = defineStore("liked", () => {
-    const likedIds = ref(new Set(JSON.parse(localStorage.getItem("liked-samples") || "[]")));
+    const storage = JSON.parse(localStorage.getItem("liked-samples") || "[]")
+    const likedIds = ref(new Set(storage));
 
     const toggleLike = (sampleId) => {
         // create temporary set to avoid direct modification to liked set
         const tempSet = new Set(likedIds.value);
 
         // toggle logic: delete if already exists in set, add if it doesn't
-        if (likedIds.value.has(sampleId)) {
+        if (tempSet.has(sampleId)) {
             tempSet.delete(sampleId)
         } else {
             tempSet.add(sampleId)
@@ -20,9 +21,11 @@ export const useLiked = defineStore("liked", () => {
 
     const isLiked = (sampleId) => likedIds.value.has(sampleId)
 
+    const likedList = computed(() => Array.from(likedIds.value))
+
     watch(likedIds, (currentSet) => {
         localStorage.setItem("liked-samples", JSON.stringify([...currentSet]))
     }, { deep: true })
 
-    return {likedIds, toggleLike, isLiked}
+    return {likedIds, likedList, toggleLike, isLiked}
 })
