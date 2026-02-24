0f2d6ce6db62dceb629139010f1d51f31f752285
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-24 15:46:35 +0100

    feat:"get token function stores token in local storage"

--- e-sample-app/src/stores/api.js
+++ e-sample-app/src/stores/api.js
@@ -10,5 +10,37 @@ export const useFreesoundAuth = () => {
         window.location.href = url; // user is redirected to freesound's page
     };
 
+    const getToken = async () => {
+        const code = new URLSearchParams(window.location.search).get("code")
+        if (code) {
+            // body constructor
+            const formData = new FormData();
+            formData.append("client_id", client_id);
+            formData.append("client_secret", client_secret);
+            formData.append("code", code)
+            formData.append("grant_type", "authorization_code")
+            console.log("formData: ", formData) // DEBUG
+
+            const res = await fetch("https://freesound.org/apiv2/oauth2/access_token/",{
+                method: "POST",
+                body: formData
+            })
+            const data = await res.json()
+            console.log("data: ", data) // DEBUG
+
+            if (data.access_token) {
+                console.log("access_token: ", data.access_token) // DEBUG
+                // stock access token in local storage
+                accessToken.value = data.access_token;
+                localStorage.setItem("fs_token", data.access_token);
+
+                localStorage.setItem("fs_refresh_token", data.refresh_token);
+
+                window.history.replaceState({}, "", "/");
+            }
+        }
+        console.log("localStorage.fs_token: ", localStorage.getItem("fs_token")); // DEBUG
+
+    }
     return {login};
 }
