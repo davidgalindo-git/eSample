d8ac66df470d9a4e546079cf3c44f17945624ed7
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-02-24 10:26:51 +0100

    import primeicons css

--- e-sample-app/src/main.js
+++ e-sample-app/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+import 'primeicons/primeicons.css'
 
 const pinia = createPinia()
 const app = createApp(App)
