39788441e6264112039345ad2d435687701e2655
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-02 15:14:50 +0100

    feat:"added assignSampleToPad function"

--- e-sample-app/src/stores/padStore.js
+++ e-sample-app/src/stores/padStore.js
@@ -4,5 +4,9 @@ import {ref} from "vue";
 export const usePadStore = defineStore('pads', () => {
     const assignedPads = ref([])
 
-    return { assignedPads };
+    const assignSampleToPad = (sample, index) => {
+        assignedPads.value = assignedPads.value.filter(pad => pad.index !== index);
+        assignedPads.value.push({index,sample});
+    }
+    return { assignedPads, assignSampleToPad };
 })
