e19f93e182a3859a02459e8e1a466f34521241aa
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-02 15:16:53 +0100

    feat:"added pad grid props and basic structure"

--- e-sample-app/src/components/pad/PadGrid.vue
+++ e-sample-app/src/components/pad/PadGrid.vue
@@ -1,8 +1,24 @@
 <script setup>
+const props = defineProps({
+  sample: {
+    type: Object,
+    default: null
+  },
+  index: {
+    type: Number,
+    required: true
+  }
+})
+
 </script>
 
 <template>
-<div class="pad-grid"></div>
+<div class="pad-grid">
+  <span class="pad-label">{{ index + 1 }}</span>
+  <div class="sample-info">
+    <p class="sample-name">{{ sample.name }}</p>
+  </div>
+</div>
 </template>
 
 <style scoped>
