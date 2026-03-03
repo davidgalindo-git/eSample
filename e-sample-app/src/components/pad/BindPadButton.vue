d2a9181efc5f0a5ed81fc59454ea5f857bf7ca0c
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-03 10:18:22 +0100

    feat:"Bind pad button component basic structure"

---
+++ e-sample-app/src/components/pad/BindPadButton.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {ref} from "vue";
+import BindPadPopUp from "./BindPadPopUp.vue";
+
+const props = defineProps({
+  sample: {
+    type: Object,
+    default: null
+  },
+  index: {
+    type: Number,
+    required: true
+  },
+  alias : {
+    type: String,
+    required: false
+  }
+})
+
+const isPadPopUp = ref(false)
+
+const togglePadPopUp = () => {
+  isPadPopUp.value = !isPadPopUp.value
+}
+
+const bindSample = () => {
+
+}
+</script>
+
+<template>
+  <BindPadPopUp
+      v-if="isPadPopUp"
+      class="pad-pop-up"
+  />
+  <button
+      class="bind-pad-button"
+      @click.stop="togglePadPopUp"
+  >
+  </button>
+</template>
+
+<style scoped>
+button {
+
+}
+
+</style>
