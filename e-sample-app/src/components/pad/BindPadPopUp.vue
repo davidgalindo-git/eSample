6704a70f3fb4a0b300f85c206bb4f150aaac004f
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-03 13:40:40 +0100

    feat:"assign pad logic"

--- e-sample-app/src/components/pad/BindPadPopUp.vue
+++ e-sample-app/src/components/pad/BindPadPopUp.vue
@@ -1,4 +1,22 @@
 <script setup>
+import {usePadStore} from "../../stores/padStore.js";
+
+const props = defineProps({
+  sample: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['close-pop-up'])
+const padStore = usePadStore()
+
+const handleAssign = (visualPos) => {
+  const logicIndex = padStore.getPadIndex(visualPos)
+  padStore.assignSampleToPad(props.sample, logicIndex, props.sample.name)
+
+  emit('close-pop-up')
+}
 
 </script>
 
