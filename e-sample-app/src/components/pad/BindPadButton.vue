236e7063dd7cf4f5fb389731132f32a1d9a07a92
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-03 10:43:42 +0100

    feat:"added basic css and props"

--- e-sample-app/src/components/pad/BindPadButton.vue
+++ e-sample-app/src/components/pad/BindPadButton.vue
@@ -1,22 +1,18 @@
 <script setup>
-import {ref} from "vue";
+const props = defineProps({
+  isPadPopUp: {
+    type: Boolean,
+    default: false
+  }
+})
 
 const emit = defineEmits(['toggle-pad-pop-up'])
-
-const isPadPopUp = ref(false)
-
-const togglePadPopUp = () => {
-  isPadPopUp.value = !isPadPopUp.value
-
-  emit("toggle-pad-pop-up", isPadPopUp.value)
-}
-
 </script>
 
 <template>
   <button
       class="bind-pad-button"
-      @click.stop="togglePadPopUp"
+      @click.stop="emit('toggle-pad-pop-up')"
       :class="{ 'is-active': isPadPopUp }"
   >
     {{ isPadPopUp ? 'x' : '+'}}
@@ -24,8 +20,26 @@ const togglePadPopUp = () => {
 </template>
 
 <style scoped>
-button {
-
+.bind-pad-button {
+  width: 30px;
+  height: 30px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  background: #444;
+  color: white;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s;
 }
 
+.bind-pad-button.is-active {
+  background: #ff4444; /* Rouge pour fermer */
+  border-color: #ff8888;
+}
+
+.bind-pad-button:hover {
+  background: #555;
+}
 </style>
