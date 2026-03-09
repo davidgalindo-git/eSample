009cfa36a7424e18bc3e25443a7c3754f0d41096
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-09 14:54:23 +0100

    fix:"Issue#48: corrected pagination logic"

--- e-sample-app/src/components/search/PageNavigator.vue
+++ e-sample-app/src/components/search/PageNavigator.vue
@@ -2,46 +2,64 @@
 import { useSampleAPI } from '../../stores/sampleStore.js';
 const sampleStore = useSampleAPI();
 
-const changePage = (newPage) => {
-  const targetPage = Number(newPage);
+const previousPage = () => {
+  sampleStore.searchSamples(sampleStore.currentQuery.value, sampleStore.currentPage.value - 1);
+}
+
+const nextPage = () => {
+  sampleStore.searchSamples(sampleStore.currentQuery.value, sampleStore.currentPage.value + 1);
+};
+
+const handleInputPage = (event) => {
+  const targetPage = Number(event.target.value);
 
   if (targetPage >= 1 && targetPage <= sampleStore.totalPages.value) {
     sampleStore.searchSamples(sampleStore.currentQuery.value, targetPage);
   }
-};
+}
 </script>
 
 <template>
-  <div class="pagination-controls">
+  <div class="pagination-controls"
+  >
     <button
-        :disabled="sampleStore.currentPage === 1"
-        @click="sampleStore.searchSamples(sampleStore.currentQuery, sampleStore.currentPage - 1)"
+        :disabled="sampleStore.currentPage.value <= 1"
+        @click="previousPage"
     >
       Previous
     </button>
 
-    <span>Page {{ sampleStore.currentPage }} out of {{ sampleStore.totalPages }}</span>
+    <span>
+      Page
+      <input
+          :key="sampleStore.currentPage"
+          type="number"
+          :value="sampleStore.currentPage.value"
+          @change="handleInputPage"
+          @keyup.enter="handleInputPage"
+          @focus="$event.target.select()"
+          style="width: 7ch; text-align: center"
+          min="1"
+          :max="sampleStore.totalPages.value"
+      >
+      / {{ sampleStore.totalPages }}
+    </span>
 
     <button
-        :disabled="sampleStore.currentPage >= sampleStore.totalPages"
-        @click="sampleStore.searchSamples(sampleStore.currentQuery, sampleStore.currentPage + 1)"
+        :disabled="sampleStore.currentPage.value >= sampleStore.totalPages.value"
+        @click="nextPage"
     >
       Next
     </button>
-
-    <div v-if="sampleStore.totalPages > 1">
-      <select
-          :value="sampleStore.currentPage"
-          @change="e => sampleStore.searchSamples(sampleStore.currentQuery, Number(e.target.value))"
-      >
-        <option v-for="p in Number(sampleStore.totalPages)" :key="p" :value="p">
-          {{ p }}
-        </option>
-      </select>
-    </div>
   </div>
 </template>
 
 <style scoped>
-
+.pagination-controls{
+  margin-bottom: 10px;
+}
+button {
+  margin-right: 20px;
+  margin-left: 20px;
+}
 </style>
