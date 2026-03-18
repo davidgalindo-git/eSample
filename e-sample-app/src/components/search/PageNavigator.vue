c4268f0dfc34f7b168da641ec1854621e98c62a9
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-03-18 15:42:39 +0100

    fix:"prevent no results case for page display"

--- e-sample-app/src/components/search/PageNavigator.vue
+++ e-sample-app/src/components/search/PageNavigator.vue
@@ -58,7 +58,7 @@ const handleInputPage = (event) => {
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
     </button>
 
-    <span>
+    <span v-if="totalPages > 0">
       Page
       <input
           :key="currentPage"
@@ -73,6 +73,7 @@ const handleInputPage = (event) => {
       >
       / {{ totalPages }}
     </span>
+    <span v-else>No results</span>
 
     <button
         class="icon-btn"
